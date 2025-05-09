76143cf4594af973568faa0245416b18e79c2710
LiviaGa <liviagallafrio1@gmail.com>
2025-05-08 22:21:34 -0300

    criação do projeto de lista de itens de comida

---
+++ app/src/main/java/LiviaGa/com/github/lista_de_compras/model/ItemModel.kt
@@ -0,0 +1,16 @@
+package LiviaGa.com.github.lista_de_compras.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+data class ItemModel(
+
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+
+    val name: String
+)
